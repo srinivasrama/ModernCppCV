ea92ab1a41b02c7ef0f84b5b0a60a4432d5fb7bf
brunoeducsantos <brunoeducsantos@gmail.com>
2022-12-06 10:03:22 +0000

    fix png read

--- StackHeapImageAllocation/task_2/src/png_strategy.cpp
+++ StackHeapImageAllocation/task_2/src/png_strategy.cpp
@@ -7,9 +7,9 @@ ImageData PngIoStrategy::Read(const std::string &file) const {
   ImageData out;
   out.rows = image.get_height();
   out.cols = image.get_width();
+  out.max_val= 255;
   for (png::uint_32 y = 0; y < image.get_height(); ++y) {
     for (png::uint_32 x = 0; x < image.get_width(); ++x) {
-      image[y][x] = png::rgb_pixel(x, y, x + y);
       std::vector<int> pix{image[y][x].red, image[y][x].green,
                            image[y][x].blue};
       out.data.emplace_back(pix);
@@ -22,7 +22,7 @@ bool PngIoStrategy::Write(const std::string &file, const ImageData &img) const {
   png::image<png::rgb_pixel> image(img.rows, img.cols);
   for (png::uint_32 i = 0; i < img.rows; i++) {
     for (png::uint_32 j = 0; j < img.cols; j++) {
-      std::vector<int> pix = img.data[i*img.cols + j];
+      std::vector<int> pix = img.data[i * img.cols + j];
       image[i][j] = png::rgb_pixel(pix[0], pix[1], pix[2]);
     }
   }

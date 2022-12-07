81189ffbe761eacd7faf33a4b5db6a2092468fc3
brunoeducsantos <brunoeducsantos@gmail.com>
2022-12-07 09:43:48 +0000

    a

---
+++ SIFT_Serial/build/tests/CMakeFiles/siftserial_test.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bruno/Learning/ModernCppCV/SIFT_Serial/tests/test_convert_dataset.cpp" "/home/bruno/Learning/ModernCppCV/SIFT_Serial/build/tests/CMakeFiles/siftserial_test.dir/test_convert_dataset.cpp.o"
+  "/home/bruno/Learning/ModernCppCV/SIFT_Serial/tests/test_main.cpp" "/home/bruno/Learning/ModernCppCV/SIFT_Serial/build/tests/CMakeFiles/siftserial_test.dir/test_main.cpp.o"
+  "/home/bruno/Learning/ModernCppCV/SIFT_Serial/tests/test_serialize.cpp" "/home/bruno/Learning/ModernCppCV/SIFT_Serial/build/tests/CMakeFiles/siftserial_test.dir/test_serialize.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/bruno/Learning/ModernCppCV/SIFT_Serial/build/CMakeFiles/convert_dataset.dir/DependInfo.cmake"
+  "/home/bruno/Learning/ModernCppCV/SIFT_Serial/build/CMakeFiles/mat_serialization.dir/DependInfo.cmake"
+  "/home/bruno/Learning/ModernCppCV/SIFT_Serial/build/tests/CMakeFiles/sifts_internal.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

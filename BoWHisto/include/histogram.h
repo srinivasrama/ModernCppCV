9e54655dc1b0ded9da148289df31c4a71004a156
brunoeducsantos <brunoeducsantos@gmail.com>
2022-12-29 15:36:46 +0000

    refactor code according to unit tests

--- BoWHisto/include/histogram.h
+++ BoWHisto/include/histogram.h
@@ -1,47 +1,51 @@
 #ifndef HISTOGRAM_HPP_
 #define HISTOGRAM_HPP_
-#include <opencv2/core.hpp>
-#include <opencv2/opencv.hpp>
-#include <vector>
 #include "bow_dictionary.h"
+#include <fstream>
+#include <iostream>
+#include <opencv2/core.hpp>
 #include <opencv2/flann.hpp>
-namespace ipb{
-class Histogram{
-    public:
-        Histogram(){};
-        Histogram(const std::vector<int> &data){
-            data_=data;
-        };
-        Histogram(const cv::Mat &descriptors, BowDictionary &dictionary);
-        //initiate all vector functionality
-        int size() const { return data_.size();}
-        bool empty() const {return !(data_.size()>0);}
-        std::vector<int> data() const{return data_;}
-        int operator[](int ind){
-            return data_[ind];
-        }
-        std::vector<int>::const_iterator begin() {
-            return data_.begin();
-        }
-        std::vector<int>::const_iterator cbegin () const {
-            return data_.cbegin();
-        }
-        std::vector<int>::const_iterator cend() const {
-            return data_.cend();
-        }
-          std::vector<int>::const_iterator end() {
-            return data_.end();
-        }
+#include <opencv2/opencv.hpp>
+#include <sstream>
+#include <vector>
+namespace ipb {
+class Histogram {
+public:
+  Histogram(){};
+  Histogram(const std::vector<int> &data) { data_ = data; };
+  Histogram(const cv::Mat &descriptors, BowDictionary &dictionary);
+  // initiate all vector functionality
+  int size() const { return data_.size(); }
+  bool empty() const { return !(data_.size() > 0); }
+  std::vector<int> data() const { return data_; }
+  int operator[](int ind) const { return data_[ind]; }
+  int &operator[](int ind) { return data_[ind]; }
 
-        // class io 
-        Histogram ReadFromCSV(const std::string &file);
-        void WriteToCSV(const std::string &file) const;
-    private:
-        void histogram(const cv::Mat &descriptors,  BowDictionary &dictionary) ;
-        std::vector<int> data_;
+  std::vector<int>::const_iterator begin() const { return data_.cbegin(); }
+  std::vector<int>::const_iterator end() const { return data_.cend(); }
 
+  std::vector<int>::const_iterator cbegin() const { return data_.cbegin(); }
+  std::vector<int>::const_iterator cend() const { return data_.cend(); }
 
+  // class io
+  static Histogram ReadFromCSV(const std::string &file);
+  void WriteToCSV(const std::string &file) const;
+  std::ostream& operator<<(std::ostream &stream) const;
+   friend std::ostream & operator <<( std::ostream& out, const Histogram& B) {
+        for (int i = 0; i < B.size() - 1; i++) {
+      out << B[i] << ", ";
+    }
+    out << B[B.size() - 1];
+    return out;
+
+    }
+
+private:
+  void histogram(const cv::Mat &descriptors, BowDictionary &dictionary);
+  std::vector<int> data_;
 };
-}
+// TODO: WHy this shit only works outside the class?
+
+} // namespace ipb
 
 #endif
--- BoWHisto/src/histogram.cpp
+++ BoWHisto/src/histogram.cpp
@@ -1,87 +1,47 @@
 #include "histogram.h"
-#include <iostream>
-#include <sstream>
-#include <fstream>
 
 namespace ipb {
 
-Histogram::Histogram(const cv::Mat &descriptors,
-                      BowDictionary &dictionary) {
-histogram(descriptors, dictionary);                        
-                     }
+Histogram::Histogram(const cv::Mat &descriptors, BowDictionary &dictionary) {
+  histogram(descriptors, dictionary);
+}
 void Histogram::histogram(const cv::Mat &descriptors,
-                                      BowDictionary &dictionary) {
+                          BowDictionary &dictionary) {
+  if(descriptors.size().height==0) return;
   data_.reserve(dictionary.size());
-  for(int i=0;i<data_.capacity();i++){
+  for (int i = 0; i < data_.capacity(); i++) {
     data_.push_back(0.);
   }
-  static cv::flann::Index flann_index(dictionary.vocabulary(), cv::flann::KDTreeIndexParams(1));
+  static cv::flann::Index flann_index(dictionary.vocabulary(),
+                                      cv::flann::KDTreeIndexParams(1));
   unsigned int max_neighbours = 10;
-  cv::Mat indices, dists; //neither assume type nor size here !
-  double radius= 2.0;
-  for(int i=0;i< descriptors.rows;++i){
-    flann_index.radiusSearch(descriptors.row(i), indices, dists, radius, max_neighbours,
-        cv::flann::SearchParams(32));
-    data_[indices.at<int>(0,0)]+=1;
-  }       
+  cv::Mat indices, dists; // neither assume type nor size here !
+  double radius =10.0;
+  for (int i = 0; i < descriptors.rows; ++i) {
+    flann_index.radiusSearch(descriptors.row(i), indices, dists, radius,
+                             max_neighbours, cv::flann::SearchParams(32));
+    std::cout << indices <<std::endl;
+    data_[indices.at<int>(0, 0)] += 1;
+  }
 }
 
-Histogram Histogram::ReadFromCSV(const std::string &file){
-    std::ifstream ifs{ file };
-     for (std::string line; std::getline(ifs, line, ','); ){
-        data_.push_back(stoi(line));
-     }
-    return Histogram(data_);
+Histogram Histogram::ReadFromCSV(const std::string &file) {
+  std::ifstream ifs{file};
+  std::vector<int> data;
+  for (std::string line; std::getline(ifs, line, ',');) {
+    data.push_back(stoi(line));
+  }
+  return Histogram(data);
 }
 
-void Histogram::WriteToCSV(const std::string &file) const{
-      std::ofstream myfile;
-      myfile.open (file);
-      for(int i=0;i<data_.size()-1;i++){
-        myfile << data_[i] <<",";
-      }
-      myfile << data_[data_.size()-1];
-      myfile.close();
+void Histogram::WriteToCSV(const std::string &file) const {
+  std::ofstream myfile;
+  myfile.open(file);
+  for (int i = 0; i < data_.size() - 1; i++) {
+    myfile << data_[i] << ",";
+  }
+  myfile << data_[data_.size() - 1];
+  myfile.close();
 }
 
 } // namespace ipb
-cv::Mat Get5Kmeans() {
-  // init some parameters
-  const int rows_num = 1;
-  const int cols_num = 10;
-  cv::Mat data;
-
-  for (int i = 0; i < 100; i += 20) {
-    cv::Mat_<float> vec(rows_num, cols_num, i);
-    data.push_back(vec);
-  }
-
-  return data;
-}
-
-cv::Mat GetAllFeatures() {
-  // init some parameters
-  const int rows_num = 1;
-  const int cols_num = 10;
-  cv::Mat data;
-
-  for (int i = 0; i < 100; i += 20) {
-    for (size_t j = 0; j < 5; j++) {
-      data.push_back(cv::Mat_<float>(rows_num, cols_num, i));
-    }
-  }
-
-  return data;
-}
-
-int main(){
-    auto& dictionary = ipb::BowDictionary::GetInstance();
-    dictionary.set_vocabulary(Get5Kmeans());
-    auto histogram_ = ipb::Histogram(GetAllFeatures(), dictionary);
-    // for(auto it=histogram_.begin();it<histogram_.end();it++){
-    //     std::cout<<"i="<<*it<<std::endl;
-    // }
-    histogram_.WriteToCSV("/home/bruno/python_scripts/food.csv");
-
-    return 0;
-}
